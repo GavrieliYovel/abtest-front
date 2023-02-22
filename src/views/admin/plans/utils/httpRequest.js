98ba77e3a343f35d5706c436e989495cb21c7509
DorHaimovich1 <dorh@usearch.com>
2023-02-22 22:33:58 +0200

    added General function for http requests

---
+++ src/views/admin/plans/utils/httpRequest.js
@@ -0,0 +1,17 @@
+const httpRequest = async (host, method, path = '', headers = {}, data = {}) => {
+    const response = await fetch(host + path, {
+        method,
+        headers,
+        body: method !== 'GET' ? JSON.stringify(data) : null
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        throw new Error(responseData.message || 'Something went wrong!');
+    }
+
+    return responseData;
+};
+
+module.exports = httpRequest;
