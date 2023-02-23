981c653d40f5ddec24ff14dde95184230d6790e2
viki bril <bril2010@gmail.com>
2023-02-23 11:39:25 +0200

    new content inside the cards

--- src/views/user/experiments/index.jsx
+++ src/views/user/experiments/index.jsx
@@ -14,7 +14,17 @@ import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json
 import tableDataColumns from "views/admin/dataTables/variables/tableDataColumns.json";
 import tableDataComplex from "views/admin/dataTables/variables/tableDataComplex.json";
 import React from "react";
+import LineAreaChart from "components/charts/BarChart"
 import Card from "components/card/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import { FcBarChart } from "react-icons/fc";
+import { RiBarChart2Line, RiBarChartGroupedFill } from "react-icons/ri";
+import { IoBarChart } from "react-icons/io5";
+import { MdBarChart } from "react-icons/md";
+
+
 
 export default function Settings() {
   // Chakra Color Mode
@@ -23,9 +33,77 @@ export default function Settings() {
 
       <Card width={"1000px"} height= {"700px"} mx={"auto"} mt={"50px"} spacing={{base: "20px", xl:"20px"}}> 
         <Button variant='darkBrand' color='white' width={"150px"} height={"40px"} spacing={{base: "20px", xl:"20px"}} fontSize='sm' fontWeight='300' ml={"auto"}> Switch to exclusive</Button> 
-        
-        {/* <Card variant="outlined" width={"800px"} height={"100px"} spacing={{base: "20px", xl:"20px"}} color='black'/> */}
+        <Card
+        style={{
+          marginTop: '50px',
+          marginLeft: '120px',
+          width: 700,
+          border: '1px solid rgba(0,0,0,0.1)',
+        }}
+      >
+        <CardContent>
+          <Typography 
+            style={{ fontSize: 14 , display: 'flex', justifyContent: 'center'}}
+            color="textSecondary"
+            gutterBottom
+            
+          >
+            First AB Experiment
+          </Typography>
+          <Typography
+            style={{
+              marginBottom: 12,
+            }}
+            color="textSecondary"
+          >
+            Type
+          </Typography>
+          <Typography color="textSecondary" component="p">
+            Status
+          </Typography>
+          <MdBarChart marginLeft='40%' />
+        </CardContent>
+        <CardActions>
+          <Button color='purple' ml='35%' style={{fontSize: 14 ,justifyContent:'center', alignItems: 'center'}}>See experiments details</Button>
+        </CardActions>
+      </Card>
 
+      <Card
+        style={{
+          marginTop: '50px',
+          marginLeft: '120px',
+          width: 700,
+          border: '1px solid rgba(0,0,0,0.1)',
+        }}
+      >
+        <CardContent>
+          <Typography 
+            style={{ fontSize: 14 , display: 'flex', justifyContent: 'center'}}
+            color="textSecondary"
+            gutterBottom
+            
+          >
+            First AB Experiment
+          </Typography>
+          <Typography
+            style={{
+              marginBottom: 12,
+            }}
+            color="textSecondary"
+          >
+            Type
+          </Typography>
+          <Typography color="textSecondary" component="p">
+            Status
+          </Typography>
+          <MdBarChart marginLeft='40%' />
+        </CardContent>
+        <CardActions>
+          <Button color='purple' ml='35%' style={{fontSize: 14 ,justifyContent:'center', alignItems: 'center'}}>See experiments details</Button>
+        </CardActions>
+      </Card>
+
+      
 
       </Card>
     </Box>
