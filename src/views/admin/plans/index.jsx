621a7577e3c7b0c3a49b31a6389e438c5ef74353
galor <galor546@gmail.com>
2023-02-27 01:49:19 +0200

    best seller set

--- src/views/admin/plans/index.jsx
+++ src/views/admin/plans/index.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import httpRequest from './utils/httpRequest';
 import {RxRocket} from 'react-icons/rx'
 import {IoMdPaperPlane} from 'react-icons/io'
 import {TbRocket} from "react-icons/tb";
+import Cookies from "js-cookie";
 
 // Chakra imports
 import {Box, Grid} from "@chakra-ui/react";
@@ -18,8 +19,12 @@ import PlanButtons from "./components/PlanButtons";
 import CustomPlan from "./components/CustomPlan";
 import PopUp from "./components/PopUp";
 
-const Plans = () => {
+import {AuthContext} from 'contexts/AuthContext';
+import {SERVER_URL} from './utils/constants';
 
+const Plans = () => {
+    const jwt = Cookies.get("jwt");
+    const {loggedInUser} = useContext(AuthContext);
     // Chakra Color Mode
     const [plans, setPlans] = useState([]);
     const [chosenPlan, setChosenPlan] = useState([]);
@@ -28,6 +33,7 @@ const Plans = () => {
     const [type, setType] = useState('');
     const [popUp, setPopUp] = useState(false);
     const [contact, setContact] = useState(false);
+    const [popular, setPopular] = useState('');
 
     const setPopUpPayment = (mode) => {
         setPayment(mode);
@@ -42,7 +48,7 @@ const Plans = () => {
     useEffect(async () => {
         const fetchPlans = async () => {
             try {
-                const response = await httpRequest('http://localhost:5000/', 'GET', 'plans');
+                const response = await httpRequest(`${SERVER_URL}`, 'GET', 'plans');
                 setPlans(response);
             } catch (err) {
                 console.log(err.message);
@@ -51,9 +57,10 @@ const Plans = () => {
 
         const fetchAccountSubDetails = async () => {
             try {
-                const response = await httpRequest('http://localhost:5000/', 'GET', 'subscriptions/63ba81cd789c4503dc2e6cc2');
+                const response = await httpRequest(`${SERVER_URL}`, 'GET', `subscriptions/${loggedInUser.accountId}`);
                 const modifyResponse = {
-                    accountId: '63c8139650166c0f99f62cbc',
+                    accountId: loggedInUser.accountId,
+                    email: loggedInUser.email,
                     ...response
                 }
                 setAccountSubDetails(modifyResponse);
@@ -63,7 +70,11 @@ const Plans = () => {
             }
         };
 
-
+        const getPopular = async () => {
+            const response = await httpRequest(`${SERVER_URL}`, 'GET', `statistics/popular`);
+            (response ? setPopular(response) : setPopular(''));
+        }
+        await getPopular();
         await fetchPlans();
         fetchAccountSubDetails();
 
@@ -86,7 +97,7 @@ const Plans = () => {
                 break;
         }
         return (
-            <Plan key={plan.name}>
+            <Plan isPopular={popular == plan.name ? true : false} key={plan.name}>
                 <PlanTitle>{plan.name}</PlanTitle>
                 <IconBox icon={icon} mt={"20px"}/>
                 <PlanFeatures features={plan.features}/>
@@ -108,8 +119,9 @@ const Plans = () => {
                 </Grid>
                 <CustomPlan setContactPopUp={setContactPopUp}/>
             </Box>
-            {popUp ? <PopUp payment={payment} accountSubDetails={accountSubDetails} setPayment={setPayment}
-                            setPopUpPayment={setPopUpPayment} contact={contact} setContactPopUp={setContactPopUp}
+            {popUp ? <PopUp payment={payment} jwt={jwt} accountSubDetails={accountSubDetails} setPayment={setPayment}
+                            setContact={setContact} setPopUpPayment={setPopUpPayment} contact={contact}
+                            setContactPopUp={setContactPopUp}
                             chosenPlan={chosenPlan} type={type}/> : null}
         </>
     );
