4cf1ea655f48a05d537b85df169ecc0a8b17eced
dkrac <dkracheli135@gmail.com>
2023-02-28 20:04:10 +0200

    final

---
+++ src/views/admin/statistics/components/DailyTraffic.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+// Chakra imports
+import {Box, Flex, Icon, Spinner, Text, useColorModeValue} from "@chakra-ui/react";
+import BarChart from "components/charts/BarChart";
+
+// Custom components
+import Card from "components/card/Card.js";
+import {
+  barChartDataDailyTraffic,
+  barChartOptionsDailyTraffic,
+} from "variables/charts";
+
+// Assets
+import { RiArrowUpSFill } from "react-icons/ri";
+
+export default function DailyTraffic(props) {
+  const { title, countries , locationCount } = props;
+  console.log(countries , locationCount);
+
+  // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  return (
+      <Card align='center' direction='column' w='100%'>
+        <Flex justify='space-between' align='start' px='10px' pt='5px'>
+          <Flex flexDirection='column' align='start' me='20px'>
+            <Flex w='100%'>
+              <Text
+                  me='auto'
+                  color='secondaryGray.800'
+                  fontSize='lg'
+                  fontWeight='500'>
+                {title}
+              </Text>
+            </Flex>
+            <Flex align='end'>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Box h='240px' mt='auto'>
+          { locationCount && countries?
+              <BarChart
+                  chartData={[{name: "value"  ,data:locationCount}]}
+                  chartOptions={barChartOptionsDailyTraffic}
+              />: <Spinner />
+          }
+        </Box>
+      </Card>
+  );
+}
---
+++ src/views/admin/statistics/components/PieCard.js
@@ -0,0 +1,65 @@
+// Chakra imports
+import {Box, Flex, Text, Select, useColorModeValue, Spinner} from "@chakra-ui/react";
+// Custom components
+import Card from "components/card/Card.js";
+import PieChart from "components/charts/PieChart";
+import { pieChartData, pieChartOptions } from "variables/charts";
+import { VSeparator } from "components/separator/Separator";
+import React from "react";
+
+export default function Conversion(props) {
+
+  const { title , devices , devicescount} = props;
+    {console.log(devicescount + "///////////////////////////////////")}
+
+  // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const cardColor = useColorModeValue("white", "navy.700");
+  const cardShadow = useColorModeValue(
+      "0px 18px 40px rgba(112, 144, 176, 0.12)",
+      "unset"
+  );
+
+  return (
+      <Card p='20px' align='center' direction='column' w='100%'>
+        <Flex
+            px={{ base: "0px", "2xl": "10px" }}
+            justifyContent='space-between'
+            alignItems='center'
+            w='100%'
+            mb='8px'>
+          <Text color={textColor} fontSize='md' fontWeight='600' mt='4px'>
+            {title}
+          </Text>
+          <Select
+              fontSize='sm'
+              variant='subtle'
+              defaultValue='monthly'
+              width='unset'
+              fontWeight='700'>
+            <option value='daily'>Daily</option>
+            <option value='monthly'>Monthly</option>
+            <option value='yearly'>Yearly</option>
+          </Select>
+        </Flex>
+        {devices && devicescount ?
+            <PieChart
+                h='100%'
+                w='100%'
+                chartData={devicescount}
+                chartOptions={pieChartOptions}
+            /> : <Spinner />
+        }
+        <Card
+            bg={cardColor}
+            flexDirection='row'
+            boxShadow={cardShadow}
+            w='100%'
+            p='15px'
+            px='20px'
+            mt='15px'
+            mx='auto'>
+        </Card>
+      </Card>
+  );
+}
